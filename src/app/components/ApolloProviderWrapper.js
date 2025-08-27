181ef3c1b2ba0d6d476e6293333185d185e61208
Ibinabo Bliss <ibinabobliss@gmail.com>
2025-08-26 17:06:09 -0700

    changed route to production

--- src/app/components/ApolloProviderWrapper.js
+++ src/app/components/ApolloProviderWrapper.js
@@ -5,7 +5,7 @@ import { ApolloProvider } from "@apollo/client/react";
 
 const client = new ApolloClient({
   link: new HttpLink({
-    uri: "http://localhost:10000/graphql", // updated GraphQL endpoint
+    uri: "https://pandora-mistress-backend.onrender.com/graphql", // updated GraphQL endpoint
   }),
   cache: new InMemoryCache(),
 });
