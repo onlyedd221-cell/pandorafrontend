406d7a974a2435a9fce4c33fcbb7104cd8d9817a
Fortune Bliss <Blissfortune222@gmail.com>
2025-09-06 01:37:49 +0100

    updated to backend url

--- src/app/components/ApolloProviderWrapper.js
+++ src/app/components/ApolloProviderWrapper.js
@@ -5,7 +5,7 @@ import { ApolloProvider } from "@apollo/client/react";
 
 const client = new ApolloClient({
   link: new HttpLink({
-    uri: " http://localhost:10000/graphql", // updated GraphQL endpoint
+    uri: "https://pandorabackend-7e67.onrender.com/graphql", // updated GraphQL endpoint
   }),
   cache: new InMemoryCache(),
 });
