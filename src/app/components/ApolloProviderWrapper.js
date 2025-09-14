8783b589e31f8895d95040a10ef48928b9ea150d
Fortune Bliss <Blissfortune222@gmail.com>
2025-09-14 13:58:03 +0100

    changed url

--- src/app/components/ApolloProviderWrapper.js
+++ src/app/components/ApolloProviderWrapper.js
@@ -5,11 +5,10 @@ import { ApolloProvider } from "@apollo/client/react";
 
 const client = new ApolloClient({
   link: new HttpLink({
-    uri: "https://pandorabackend-7e67.onrender.com/graphql", // ✅ correct spelling
+    uri: "https://pandora-mistress-backend.onrender.com/graphql", // updated GraphQL endpoint
   }),
   cache: new InMemoryCache(),
 });
-
 
 export default function ApolloProviderWrapper({ children }) {
   return <ApolloProvider client={client}>{children}</ApolloProvider>;
